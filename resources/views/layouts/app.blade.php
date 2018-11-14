b205202f9643480e2fbfdefc7cc091ab0abeefac
binhnguyen1998822 <32400888+binhnguyen1998822@users.noreply.github.com>
2018-11-14 16:40:17 +0700

    JS History

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,32 +12,10 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ url('assets/css/bootstrap.min.css') }} " rel="stylesheet" />
-    <!--  Material Dashboard CSS    -->
-    <link href="{{ url('assets/css/material-dashboard.css?v=1.2.0') }} " rel="stylesheet" />
-    <!--  CSS for Demo Purpose, don't include it in your project     -->
-    <link href="{{ url('assets/css/demo.css') }} " rel="stylesheet" />
-    <!--     Fonts and icons     -->
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
-    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700,300|Material+Icons' rel='stylesheet' type='text/css'>
-    <!-- Add jQuery library -->
-    <script type="text/javascript" src="{{ asset('lib/jquery-1.10.2.min.js') }}"></script>
 </head>
 
 <body>
         @yield('content')
 
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}"></script>
 </body>
-<script src="{{ asset('assets/js/material.min.js') }}" type="text/javascript"></script>
-<!--  Charts Plugin -->
-<script src="{{ asset('assets/js/chartist.min.js') }}"></script>
-<!--  Dynamic Elements plugin -->
-<script src="{{ asset('assets/js/arrive.min.js') }}"></script>
-<!--  PerfectScrollbar Library -->
-<script src="{{ asset('assets/js/perfect-scrollbar.jquery.min.js') }}"></script>
-<!--  Notifications Plugin    -->
-<script src="{{ asset('assets/js/bootstrap-notify.js') }}"></script>
-<script src="{{ asset('assets/js/material-dashboard.js?v=1.2.0') }}"></script>
 </html>
